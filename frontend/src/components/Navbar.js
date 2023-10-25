e91432a896ca06fc8963f435d51edf2f793bbb79
rasmolina <robertoasmolin@gmail.com>
2023-10-25 20:20:54 -0300

    correção navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Importe o Link
 import logoCorreios from '../components/logoCorreios.png';
 
 export default function Navbar() {
+  const handleAddUserClick = () => {
+    window.location.href = '/add-user';
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <Link to="/" className="navbar-brand" style={{ marginLeft: '10px' }}>
-        <img src={logoCorreios} alt="Logo Correios" width="auto" height="50" />
-      </Link>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item active"></li>
-        </ul>
-      </div>
-      <div className="ml-auto">
-        <Link to="/add-user" className="btn btn-warning" style={{ marginRight: '15px' }}>
-          Adicionar Usuário
-        </Link>
-      </div>
-      <div className="ml-auto">
-        <button className="btn btn-warning" style={{ marginRight: '15px' }}>Ver encomendas</button>
-      </div>
-    </nav>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a className="navbar-brand" href="/" style={{ marginLeft: '10px' }}>
+          <img src={logoCorreios} alt="Logo Correios" width="auto" height="50" />
+        </a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+            </li>
+          </ul>
+        </div>
+        <div className="ml-auto">
+          <button className="btn btn-warning" style={{ marginRight: '15px' }} onClick={handleAddUserClick}>
+            Adicionar Usuário
+          </button>
+        </div>
+        <div className="ml-auto">
+          <button className="btn btn-warning" style={{ marginRight: '15px' }}>Ver encomendas</button>
+        </div>
+      </nav>
+    </div>
   );
 }
